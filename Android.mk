7536caa651ea224e612c8be2ecb5f712a55631bd
Adrian Negreanu <adrian.negreanu@nxp.com>
2019-02-15 11:55:37 +0200

    MGS-4519 [#ccc]: write a functioning Android.mk
    Signed-off-by: Adrian Negreanu <adrian.negreanu@nxp.com>

--- Android.mk
+++ Android.mk
@@ -1,17 +1,25 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := gputop
+LOCAL_MODULE := gpu-top
 
-LOCAL_C_INCLUDES += ../gputop ${GPUPERFCNT_INCLUDE_PATH}
-LOCAL_CFLAGS += -Wall -Wextra -Werror -Wstrict-prototypes -Wmissing-prototypes \
-		-std=c99 -O2 -UNDEBUG -DHAVE_DDR_PERF
+LOCAL_C_INCLUDES += \
+  $(TOP)/external/libgpuperfcnt/include/ \
+  $(TOP)/external/gputop/
 
-LOCAL_SHARED_LIBRARIES += gpuperfcnt
-LOCAL_LDLIBS += -lgpuperfcnt
-LOCAL_LDFLAGS += -L${GPUPERFCNT_LIB_PATH}
+LOCAL_CFLAGS += \
+  -Wall -Wextra -Werror \
+  -Wstrict-prototypes \
+  -Wmissing-prototypes \
+  -std=c99 \
+  -O2 \
+  -UNDEBUG \
+  -DHAVE_DDR_PERF
 
-LOCAL_SRC_FILES := ../gputop/debugfs.c	\
-		   ../gputop/top.c
+LOCAL_SHARED_LIBRARIES += libgpuperfcnt
+
+LOCAL_SRC_FILES := \
+  gputop/debugfs.c \
+  gputop/top.c
 
 include $(BUILD_EXECUTABLE)
