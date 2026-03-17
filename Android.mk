6f2fbae58a90db4c82c4dc25db449dd25344f263
Xuegang Liu <xuegang.liu@nxp.com>
2026-03-17 14:05:02 +0800

    MA-24594 Support build gputop from source code
    Support build gputop from source code on Android, not use
    absolute path, control the build for imx8 and imx9.
    
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- Android.mk
+++ Android.mk
@@ -1,12 +1,11 @@
-ifneq ($(filter true,$(PREBUILT_FSL_IMX_GPU) $(PREBUILT_FSL_IMX_GPU_MALI)),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := gpu-top
 
 LOCAL_C_INCLUDES += \
-  $(TOP)/vendor/nxp-private/libgpuperfcnt/include/ \
-  $(TOP)/vendor/nxp-private/gputop/
+  $(LOCAL_PATH)/../libgpuperfcnt/include \
+  $(LOCAL_PATH)/gputop
 
 LOCAL_CFLAGS += \
   -Wall -Wextra -Werror \
@@ -18,7 +17,12 @@ LOCAL_CFLAGS += \
 LOCAL_STATIC_LIBRARIES += libgpuperfcnt
 
 # MALI_GPU: 1 for imx95, 0 for imx8
+ifeq ($(BOARD_SOC_CLASS),IMX8)
 MALI_GPU ?= 0
+else ifeq ($(BOARD_SOC_CLASS),IMX9)
+MALI_GPU ?= 1
+endif
+
 ifeq ($(MALI_GPU), 1)
 LOCAL_CPPFLAGS += -std=c++14
 LOCAL_CFLAGS += -DHAVE_GPU_MALI=1
@@ -35,4 +39,3 @@ endif
 LOCAL_VENDOR_MODULE  := true
 LOCAL_MODULE_TAGS    := optional
 include $(BUILD_EXECUTABLE)
-endif
